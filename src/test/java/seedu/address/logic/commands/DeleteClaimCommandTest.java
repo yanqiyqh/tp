c1f571c6ea63e9f9b7589b4c68da76f9a4dd1c01
yanqiyqh <yanqi.yqh@gmail.com>
2024-10-19 22:58:23 +0800

    Add yet-to-be-functional success tests execute_validIndexUnfilteredList_success() and execute_validIndexFilteredList_success()

--- src/test/java/seedu/address/logic/commands/DeleteClaimCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteClaimCommandTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showClientAtIndex;
 import static seedu.address.testutil.TypicalClients.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CLIENT;
@@ -20,11 +21,15 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.LogicManager;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.client.Client;
 import seedu.address.model.client.exceptions.ClaimException;
 import seedu.address.model.client.exceptions.InsurancePlanException;
+import seedu.address.model.client.insurance.InsurancePlan;
+import seedu.address.model.client.insurance.claim.Claim;
 import seedu.address.storage.JsonAddressBookStorage;
 import seedu.address.storage.JsonUserPrefsStorage;
 import seedu.address.storage.StorageManager;
@@ -53,8 +58,23 @@ public class DeleteClaimCommandTest {
 
     @Test
     public void execute_validIndexUnfilteredList_success() throws CommandException, InsurancePlanException, ClaimException {
-    }
 
+        ModelManager expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+
+        Client clientToEdit = model.getFilteredClientList().get(INDEX_FOURTH_CLIENT.getZeroBased());
+        InsurancePlan insurancePlan = clientToEdit.getInsurancePlansManager().getInsurancePlan(0); // Assuming 0 as insurance ID
+        Claim claim = clientToEdit.getInsurancePlansManager().getInsurancePlan(0).getClaim("A1001");
+
+        DeleteClaimCommand deleteClaimCommand = new DeleteClaimCommand(INDEX_FOURTH_CLIENT, 0, claim.getClaimId());
+
+        String expectedMessage = String.format(DeleteClaimCommand.MESSAGE_DELETE_CLAIM_SUCCESS,
+                clientToEdit.getName().toString(), insurancePlan, claim.getClaimId());
+
+        expectedModel.getFilteredClientList().get(INDEX_FOURTH_CLIENT.getZeroBased())
+                .getInsurancePlansManager().deleteClaimFromInsurancePlan(insurancePlan, claim);
+
+        assertCommandSuccess(deleteClaimCommand, model, expectedMessage, expectedModel);
+    }
 
     @Test
     public void execute_invalidIndexUnfilteredList_throwsCommandException() {
@@ -66,6 +86,23 @@ public class DeleteClaimCommandTest {
 
     @Test
     public void execute_validIndexFilteredList_success() throws CommandException, InsurancePlanException, ClaimException {
+        showClientAtIndex(model, INDEX_FOURTH_CLIENT);
+
+        Client clientToEdit = model.getFilteredClientList().get(INDEX_FIRST_CLIENT.getZeroBased());
+        InsurancePlan insurancePlan = clientToEdit.getInsurancePlansManager().getInsurancePlan(0);
+        Claim claim = insurancePlan.getClaim("B1122");
+
+        DeleteClaimCommand deleteClaimCommand = new DeleteClaimCommand(INDEX_FIRST_CLIENT, 0, claim.getClaimId());
+
+        String expectedMessage = String.format(DeleteClaimCommand.MESSAGE_DELETE_CLAIM_SUCCESS,
+                clientToEdit.getName().toString(), insurancePlan.getInsurancePlanId(), claim.getClaimId());
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.getFilteredClientList().get(INDEX_FIRST_CLIENT.getZeroBased())
+                .getInsurancePlansManager().deleteClaimFromInsurancePlan(insurancePlan, claim);
+        showNoClient(expectedModel);
+
+        assertCommandSuccess(deleteClaimCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
